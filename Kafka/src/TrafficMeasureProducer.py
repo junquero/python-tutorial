96357808791ffd6d808f91bf352901028b9239bf
Ilde <junquero@gmail.com>
2025-07-24 18:22:07 +0200

    Produce messages with AVRO automatic serialization.

--- Kafka/src/TrafficMeasureProducer.py
+++ Kafka/src/TrafficMeasureProducer.py
@@ -1,69 +1,79 @@
-import confluent_kafka
-import avro.schema
-from avro.io import DatumWriter, BinaryEncoder
+import random
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka import SerializingProducer
 import time
-import io
-from confluent_kafka.schema_registry import SchemaRegistryClient, SchemaRegistryError, Schema
 
 class TrafficMeasureProducer:
+    def create_schema_registry_client(self):
+        schema_registry_conf = {'url': 'http://localhost:8081'}
+        return SchemaRegistryClient(schema_registry_conf)
+    
+    # Define how to convert your dict to something AvroSerializer can use
+    def dict_to_avro(self,obj, ctx):
+        return obj
+
+    def create_producer(self):
+
+        # Read Avro schema as string
+        with open('./src/TrafficMeasure.avsc', 'r') as f:
+            schema_str = f.read()
+
+
+        avro_serializer = AvroSerializer(
+            self.schema_registry_client,
+            schema_str,
+            self.dict_to_avro
+        )
+
+        producer_conf = {
+            'bootstrap.servers': 'localhost:9092',
+#            'key.serializer': StringSerializer('utf_8'),
+            'value.serializer': avro_serializer,
+            'group.id': 'traffic-measure-producer'
+        }
+
+        return SerializingProducer(producer_conf)
+
     def __init__(self, topic: str):
-        self.producer = confluent_kafka.Producer({
-            "bootstrap.servers": "localhost:9092",
-            "group.id": "traffic-measure-producer",
-        })
+        self.schema_registry_client = self.create_schema_registry_client()
+        self.producer = self.create_producer()
         self.topic = topic
-        self.schema_str = open("./src/TrafficMeasure.avsc", "r").read()
-        self.schema = avro.schema.parse(self.schema_str)
-        self.schema_registry_client = SchemaRegistryClient({
-            "url": "http://localhost:8081"
-        })
-        # Catch the error if the schema is not registered
-        try:
-            # Check if the schema is already registered
-            latest_version = self.schema_registry_client.get_latest_version(self.topic)
-            # Get schema id from the latest version
-            self.schema_id = latest_version.schema_id
-            print(f"Got existing Schema id: {self.schema_id}")
-        except SchemaRegistryError as e:
-            # Register the schema
-            self.schema_id = self.schema_registry_client.register_schema(
-                self.topic,
-                Schema(self.schema_str, "AVRO")
-            )
-            print(f"Registered Schema id: {self.schema_id}")
 
 
     def produce(self, traffic_measure):
-        # Producer the traffic measure to the Kafka topic as avro format
-        # Create a writer for the avro schema
-        writer = DatumWriter(self.schema)
-        # Create a buffer for the avro data
-        buffer = io.BytesIO()
-        encoder = BinaryEncoder(buffer) 
-        # Write the traffic measure to the buffer
-        writer.write(traffic_measure, encoder)
-        # Produce the traffic measure to the Kafka topic
-        self.producer.produce(self.topic, buffer.getvalue(), schema_id=self.schema_id)
-        print(f"Produced traffic measure to the Kafka topic: {self.topic}")
+        self.producer.produce(
+            topic=self.topic,
+            key=str(traffic_measure["timestamp"]),
+            value=traffic_measure,
+            headers=[("organization", "CNP"), ("source", "traffic-measure-producer")]
+        )
     
     def close(self):
         self.producer.flush()
 
 
 def main():
-    producer = TrafficMeasureProducer("traffic-measures")
-    # Create a TrafficMeasure object
-    traffic_measure = {
-            "timestamp": int(time.time() * 1000),
-            "average_speed": 40.0,
-            "vehicle_count": 10,
-            "occupancy_percent": 0.5,
-            "location_geojson": "POINT(10.0 10.0)" }
+    # Create a TrafficMeasureProducer object
+    tm_producer = TrafficMeasureProducer("traffic-measures")
 
-    # Produce the traffic measure to the Kafka topic
-    producer.produce(traffic_measure)
+    # Produce batch of traffic measures
+    for i in range(100):
+        # Create a TrafficMeasure object
+        traffic_measure = {
+                "timestamp": int(time.time() * 1000),
+                "average_speed": random.randint(0, 100),
+                "vehicle_count": random.randint(0, 100),
+                "occupancy_percent": random.randint(0, 100),
+                "location_geojson": "POINT(" + str(random.randint(0, 1000)/10.0) + " " + str(random.randint(0, 1000)/10.0) + ")" }
 
-    producer.close()
+        # Produce the traffic measure to the Kafka topic
+        tm_producer.produce(traffic_measure)
+
+    print(f"Produced traffic measures to the Kafka topic: {tm_producer.topic}")
+
+    # Close the producer
+    tm_producer.close()
 
 if __name__ == "__main__":
     main()
